cfe4f161dae920e5fc68ff5e63623c8ff8844b9a
Laurent Pinsivy <lpinsivy@merethis.com>
2014-03-11 16:22:10 +0000

    add html theme

--- doc/conf.py
+++ doc/conf.py
@@ -89,9 +89,20 @@ pygments_style = 'sphinx'
 
 # -- Options for HTML output ---------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-html_theme = 'default'
+if on_centreon_rtd:
+	# The theme to use for HTML and HTML Help pages.  See the documentation for
+	# a list of builtin themes.
+	html_theme = 'centreon'
+
+	# Theme options are theme-specific and customize the look and feel of a theme
+	# further.  For a list of options available for each theme, see the
+	# documentation.
+	#html_theme_options = {}
+
+	# Add any paths that contain custom themes here, relative to this directory.
+	html_theme_path = ['/srv/themes']
+else:
+	html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
